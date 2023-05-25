d509c846e48585ffd0ad515719877f5b5168e80d
wbandersonjr <wbandersonjr@aol.com>
2023-05-25 09:45:40 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -12,20 +12,20 @@ validateEntries = function () {
         alert('running for in loop');
         let elem = Entries[x];
         alert(elem);
-        if (elem.match(/^(0|[1-9]\d*)?(\.\d+)?(?<=\d)$/)) {
-            let target = "error_";
-            target = target.concat(Entries[x]);
-            document.getElementById(target).style.display = "none";
-        } else {
-            alert('entered false snippet or regex check');
-            let target = "error_";
-            target = target.concat(Entries[x]);
-            document.getElementById(target).style.display = "block";
-            document.getElementById("goal").value = 0;
-            document.getElementsByTagName("p")[0].style.display = "none";
-            document.getElementsByTagName("p")[1].style.display = "none";
-            count += 1;
-        };
+        // if (elem.match(/^(0|[1-9]\d*)?(\.\d+)?(?<=\d)$/)) {
+        //     let target = "error_";
+        //     target = target.concat(Entries[x]);
+        //     document.getElementById(target).style.display = "none";
+        // } else {
+        //     alert('entered false snippet or regex check');
+        //     let target = "error_";
+        //     target = target.concat(Entries[x]);
+        //     document.getElementById(target).style.display = "block";
+        //     document.getElementById("goal").value = 0;
+        //     document.getElementsByTagName("p")[0].style.display = "none";
+        //     document.getElementsByTagName("p")[1].style.display = "none";
+        //     count += 1;
+        // };`
     };
 //     if (count == 0) {
 //         calcFinal ();
