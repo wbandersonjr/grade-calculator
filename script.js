14793280c363d20947af7beb9245ed098f045606
wbandersonjr <wbandersonjr@aol.com>
2023-05-31 12:08:12 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,82 +1,106 @@
 
-document.getElementById("goal").addEventListener("change",function () {validateEntries();});
 
-
-validateEntries = function () {
-    const Entries = ["t1", "t2", "se1", "t3", "t4"];
+validateEntries = function (which) {
     let count = 0;
-    for (let x in Entries) {
-        let elem = document.getElementById(Entries[x]).value;
-// regex lookback not working on mobile browsers
-
+    for (let i = 0; i < Grades.length-1; i++) {
+        let elem = document.getElementById(Grades[i][0]).value;
         let regex = /^(\d*)(\.?)(\d+)$/;
-        if (regex.test(elem)) {
+        if (regex.test(elem) && elem>0 && elem<100.001) {
             let target = "error_";
-            target = target.concat(Entries[x]);
+            target = target.concat(Grades[i][0]);
             document.getElementById(target).style.display = "none";
         } else {
             let target = "error_";
-            target = target.concat(Entries[x]);
+            target = target.concat(Grades[i][0]);
             document.getElementById(target).style.display = "block";
             document.getElementById("goal").value = 0;
             document.getElementsByTagName("p")[0].style.display = "none";
             document.getElementsByTagName("p")[1].style.display = "none";
             count += 1;
-        };
-    };
+        };        
+    }
     if (count == 0) {
-        calcFinal ();
+        calcFinal (which);
     };
 };
 
-calcFinal = function () {
-    t1 = Number(document.getElementById("t1").value);
-    t2 = Number(document.getElementById("t2").value);
-    se1 = Number(document.getElementById("se1").value);
-    t3 = Number(document.getElementById("t3").value);
-    t4 = Number(document.getElementById("t4").value);
-    val = Number(document.getElementById("goal").value);
 
-    culmGr = 0.2*(t1+t2+t3+t4)+0.1*se1;
-    grCurr = 0.2*(t1+t2)+0.1*(se1)+0.25*(t3+t4);
-    grCurr = Number(grCurr).toFixed(2);
+calcFinal = function (which) {
+    
+    
+    let culmGr = 0;
+
+    for (let i = 0; i < Grades.length-1; i++) {
+        console.log(document.getElementById(Grades[i][0]).value);
+        console.log(Grades[i][1]);
+        culmGr += document.getElementById(Grades[i][0]).value*Grades[i][1];
+        console.log("culmGr = " + culmGr);
+
+    } 
+
+    let grCurr = 0;
+
+    switch (which) {
+        case 1:
+            console.log("year calc");
+            for (let i = 0; i < Grades.length-3; i++) {
+                grCurr += document.getElementById(Grades[i][0]).value*Grades[i][1];
+            }
+            for (let i = Grades.length-3; i < Grades.length-1; i++) {
+                grCurr += document.getElementById(Grades[i][0]).value*0.25;
+                console.log("grCurr = " + grCurr);
+            };
+            break;
+        case 2:
+            console.log("semester calc");
+            for (let i = 0; i < Grades.length-1; i++) {
+                grCurr += document.getElementById(Grades[i][0]).value*0.5;
+                console.log("grCurr = " + grCurr);
+            };
+            break;
+    };
+    grCurr = grCurr.toFixed(2);
+
+    let val = Number(document.getElementById("goal").value);
+    let lastIndex = Grades.length-1;
+    console.log(val);
     let lowest = "F";
 
-    if (culmGr >= 89.6) {
+    if (culmGr >= 89.5) {
         lowest = "an A";
-    } else if (culmGr >= 79.6) {
+    } else if (culmGr >= 79.5) {
         lowest = "a B";
-    } else if (culmGr >= 69.6) {
+    } else if (culmGr >= 69.5) {
         lowest = "a C";
-    } else if (culmGr >= 59.6) {
+    } else if (culmGr >= 59.5) {
         lowest = "a D";
-    } else if (culmGr >= 39.6) {
+    } else if (culmGr >= 39.5) {
         lowest = "an E";
     };
     switch (val) {
         case 0:
             break;
         case 1:
-            final = (89.5-culmGr)/0.1;
+            final = (89.5-culmGr)/Grades[lastIndex][1];
             break;
         case 2:
-            final = (79.5-culmGr)/0.1;
+            final = (79.5-culmGr)/Grades[lastIndex][1];
             break;
         case 3:
-            final = (69.5-culmGr)/0.1;
+            final = (69.5-culmGr)/Grades[lastIndex][1];
             break;
         case 4:
-            final = (59.5-culmGr)/0.1;
+            final = (59.5-culmGr)/Grades[lastIndex][1];
             break;
         case 5:
-            final = (39.5-culmGr)/0.1;
+            final = (39.5-culmGr)/Grades[lastIndex][1];
     };
     final = Number(final).toFixed(2);
     
     document.getElementById("grOverall").style.display = "block";
     document.getElementById("scoreFinal").style.display = "block";
     document.getElementById("grade_current").innerHTML = grCurr;
-    document.getElementById("se2").innerHTML = final;
+    document.getElementById("se").innerHTML = final;
     if (final < 0) {
         document.getElementById("comment").innerHTML = "! You cannot possibly get less than " + lowest + ".";
     } else if (final > 100) {
@@ -87,11 +111,25 @@ calcFinal = function () {
     };
 };
 
+changeExempt = function () {
+    if (document.getElementById("se1_exempt").checked) {
+        document.getElementById("se1").disabled=true;
+        document.getElementById("se1").value="";
+        document.getElementById("error_se1").style.display="none";
+        Grades[0][1]=0.25;
+        Grades[1][1]=0.25;
+        Grades.splice(2,1);
+        console.log(Grades);
+    } else {
+        document.getElementById("se1").disabled=false;
+        Grades[0][1]=0.2;
+        Grades[1][1]=0.2;
+        Grades.splice(2,0,["se1",0.1]);
+        console.log(Grades);
+    };
+};
 
 
 
-document.getElementById("t1").addEventListener("change", function () {if(document.getElementById("goal").value!=0){validateEntries();}}); 
-document.getElementById("t2").addEventListener("change", function () {if(document.getElementById("goal").value!=0){validateEntries();}}); 
-document.getElementById("se1").addEventListener("change", function () {if(document.getElementById("goal").value!=0){validateEntries();}}); 
-document.getElementById("t3").addEventListener("change", function () {if(document.getElementById("goal").value!=0){validateEntries();}}); 
-document.getElementById("t4").addEventListener("change", function () {if(document.getElementById("goal").value!=0){validateEntries();}});
+
+
